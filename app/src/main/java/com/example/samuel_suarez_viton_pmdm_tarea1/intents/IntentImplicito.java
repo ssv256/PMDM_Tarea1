1bf931ba3621c097b7b9fe82b5339783b60068bb
samuel <samuel250605@gmail.com>
2024-10-10 12:43:10 +0200

    Primer commit con el diseño del activity main

---
+++ app/src/main/java/com/example/samuel_suarez_viton_pmdm_tarea1/intents/IntentImplicito.java
@@ -0,0 +1,26 @@
+package com.example.samuel_suarez_viton_pmdm_tarea1.intents;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.samuel_suarez_viton_pmdm_tarea1.R;
+
+public class IntentImplicito extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_intent_implicito);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
